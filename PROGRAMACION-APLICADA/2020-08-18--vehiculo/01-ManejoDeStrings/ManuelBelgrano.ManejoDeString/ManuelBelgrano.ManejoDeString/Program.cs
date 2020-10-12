3c30ba5acad5837d70a771aae781f45ef2e7852f
naihloan <benjaminjuarezarlt@gmail.com>
2020-10-12 12:02:05 -0300

    materiales de programacion aplicada incluyendo vehiculo teleprompter y ordenamiento

---
+++ PROGRAMACION-APLICADA/2020-08-18--vehiculo/01-ManejoDeStrings/ManuelBelgrano.ManejoDeString/ManuelBelgrano.ManejoDeString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManuelBelgrano.ManejoDeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string miString = "Mis archivos están en el direcotorio c:\\Mis Archivos";
+            //string miString = "El personaje dijo de forma irónica \"hola\"";
+            ///string miString = "Querido hermano:\n\t\t\tTe escribo esta carta...";
+            //string miString = string.Format("{0}", "hola");
+            //string miString = string.Format("Modelo:{1} - Marca {0}", "BMW",1990);
+            string miString = string.Format("Modelo:{1} - Marca {0} - Precio: {2:C}", "BMW", 1990,195000.25);
+            Console.WriteLine(miString);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PROGRAMACION-APLICADA/2020-08-18--vehiculo/02-ManuelBelgrano.Vehiculo/ManuelBelgrano.Vehiculo/ManuelBelgrano.Vehiculo/Auto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManuelBelgrano.Vehiculo
+{
+    public class Auto : IVehiculo
+    {
+        public void Acelerar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Frenar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MostrarForma()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
