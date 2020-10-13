25bcc44c9c07c064e2520a4fca12268e0c672432
venhamon <benjaminjuarezarlt@gmail.com>
2020-10-13 20:52:22 -0300

    sistema de datos actualizacion de materiales

---
+++ PROGRAMACION-APLICADA/2020-08-19--teleprompter/02-Resuelto/ManuelBelgrano/ManuelBelgrano.Aplicacion/ProgramaTeleprompter2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+namespace ManuelBelgrano.Aplicacion
+{
+    public class ProgramaTeleprompter : IProgramaConsola
+    {
+        public void IniciarPrograma()
+        {
+            Console.Clear();
+            string[] text = File.ReadAllLines("prompt.txt");
+            var tamanio = text.Length;
+            Console.WriteLine(text);
+            
+          
+
+            
+        }
+    }
+}
---
+++ PROGRAMACION-APLICADA/testOutputWords/PoemaDetalle/PoemaDetalle/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace PoemaDetalle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            var poem = File.ReadAllLines("crunch.txt");
+            // Console.WriteLine("Hello World!");
+            // Console.WriteLine(poem[0]);
+            richTextBox1.Text = poem;
+        }
+    }
+}
