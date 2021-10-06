a7590d994a85da26d69b821f2d2b5dff2c534854
venhamon <benjaminjuarezarlt@gmail.com>
2021-10-06 19:13:18 -0300

    mvc

---
+++ PAI-II--Borras/2doSemestre/2doParcial/Examen/Examen/Codigo/ManuelBelgrano/ManuelBelgrano.DataAccess/Repository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+namespace ManuelBelgrano.DataAccess
+{
+    public class Repository
+    {
+        public DataTable GetAllRecords(string tableName)
+        {
+            var cnnHandler = new ConnectionHandler();//Handler
+
+            var query = $"select * from {tableName}";
+            //seteo el data adapter
+            var da = cnnHandler.GetDataAdpter(query);
+            var dataTable = new DataTable();
+            da.Fill(dataTable);
+
+            cnnHandler.CloseConnection();
+            return dataTable;
+        }
+    }
+}
